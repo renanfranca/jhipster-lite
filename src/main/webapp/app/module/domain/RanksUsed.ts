4d93412c2bb037d5bb5d0e602895f1b11fcf4a5e
Renan Franca <renan.andradefranca@gmail.com>
2025-02-12 11:59:18 -0300

    feat: disable rank button without module rank associated

--- src/main/webapp/app/module/domain/RanksUsed.ts
+++ src/main/webapp/app/module/domain/RanksUsed.ts
@@ -1,7 +1,27 @@
 import { RankUsed } from '@/module/domain/RankUsed';
+import { Landscape } from '@/module/domain/landscape/Landscape';
+import { ModuleRank, RANKS } from '@/module/domain/landscape/ModuleRank';
 
 export type RanksUsed = {
   ranksUsed: RankUsed[];
 };
 
-// export const toRanksUsed = (landscape: Landscape): RanksUsed => {};
+export const toRanksUsed = (landscape: Landscape): RanksUsed => {
+  const rankCounts = new Map<ModuleRank, number>(RANKS.map(rank => [rank, 0]));
+
+  landscape
+    .standaloneLevels()
+    .flatMap(level => level.elements)
+    .flatMap(element => element.allModules())
+    .forEach(module => {
+      const currentCount = rankCounts.get(module.rank()) || 0;
+      rankCounts.set(module.rank(), currentCount + 1);
+    });
+
+  return {
+    ranksUsed: RANKS.map(rank => ({
+      rank,
+      quantity: rankCounts.get(rank) || 0,
+    })),
+  };
+};
