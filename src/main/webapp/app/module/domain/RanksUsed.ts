06a2bf496bb721024d79186f6f88c9d66fe9bcaf
Renan Franca <renan.andradefranca@gmail.com>
2025-02-12 11:47:40 -0300

    feat: disable rank button without module rank associated

---
+++ src/main/webapp/app/module/domain/RanksUsed.ts
@@ -0,0 +1,7 @@
+import { RankUsed } from '@/module/domain/RankUsed';
+
+export type RanksUsed = {
+  ranksUsed: RankUsed[];
+};
+
+// export const toRanksUsed = (landscape: Landscape): RanksUsed => {};
